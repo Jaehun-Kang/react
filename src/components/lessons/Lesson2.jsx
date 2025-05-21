6306e2e171426c24609aaac8d8fdb2bfc84f0d41
Leeyoungmin1 <163329389+Leeyoungmin1@users.noreply.github.com>
2025-05-21 20:27:47 +0900

    tt

---
+++ src/components/lessons/Lesson2.jsx
@@ -0,0 +1,20 @@
+import '../../styles/lesson.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const Lesson2 = {
+  name: '2. 프로젝트 명령어 사용하기',
+  idx: 2,
+  content: (
+    <div className="lesson-content">
+      <h2>tnwjd</h2>
+      <p>내용</p>
+      <SyntaxHighlighter language="jsx" style={oneDark}>
+        {/* `` 안에 코드 */}
+        {`코드`}
+      </SyntaxHighlighter>
+    </div>
+  ),
+};
+
+export default Lesson2;
