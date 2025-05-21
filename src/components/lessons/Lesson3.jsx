f43dab6d3e2d3689733842c2f524e3473a69d04a
Leehyunmin04 <163329300+Leehyunmin04@users.noreply.github.com>
2025-05-21 20:55:50 +0900

    ㅇㅁㅇ

--- src/components/lessons/Lesson3.jsx
+++ src/components/lessons/Lesson3.jsx
@@ -3,20 +3,18 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Lesson3 = {
-    name: '3. 리액트 프로젝트 살펴보기',
-    idx: 3,
-    content: (
-        <div className="lesson-content">
-            <h2>리액트 프로젝트 살펴보기</h2>
-            <p>내용</p>
-            <SyntaxHighlighter language="jsx" style={oneDark}>
-                {/* `` 안에 코드 */}
-{
-`코드`
-}
-            </SyntaxHighlighter>
-        </div>
-    )
+  name: '3. 리액트 프로젝트 살펴보기',
+  idx: 3,
+  content: (
+    <div className="lesson-content">
+      <h2>리액트 프로젝트 살펴보기</h2>
+      <p>왜 안될까?</p>
+      <SyntaxHighlighter language="jsx" style={oneDark}>
+        {/* `` 안에 코드 */}
+        {`코드`}
+      </SyntaxHighlighter>
+    </div>
+  ),
 };
 
 export default Lesson3;
