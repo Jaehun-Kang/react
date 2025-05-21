db0c5329c38bc7a6c3118d7d53aabed9034f7c8e
Jaehun-Kang <74220533+Jaehun-Kang@users.noreply.github.com>
2025-05-21 19:46:49 +0900

    파일분리

--- react-study/src/components/lessons/Lesson1.jsx
+++ react-study/src/components/lessons/Lesson1.jsx
@@ -3,20 +3,18 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Lesson1 = {
-    name: '1. 프로젝트 생성하기',
-    idx: 1,
-    content: (
-        <div className="lesson-content">
-            <h2>프로젝트 생성하기</h2>
-            <p>내용</p>
-            <SyntaxHighlighter language="jsx" style={oneDark}>
-                {/* `` 안에 코드 */}
-{
-`코드`
-}
-            </SyntaxHighlighter>
-        </div>
-    )
+  name: '1. 프로젝트 생성하기',
+  idx: 1,
+  content: (
+    <div className="lesson-content">
+      <h2>프로젝트 생성하기</h2>
+      <p>내용</p>
+      <SyntaxHighlighter language="jsx" style={oneDark}>
+        {/* `` 안에 코드 */}
+        {`hello`}
+      </SyntaxHighlighter>
+    </div>
+  ),
 };
 
 export default Lesson1;
--- react-study/src/components/lessons/index.js
+++ react-study/src/components/lessons/index.js
@@ -3,13 +3,9 @@ import Lesson1 from './Lesson1';
 import Lesson2 from './Lesson2';
 import Lesson3 from './Lesson3';
 import Lesson4 from './Lesson4';
+import Lesson4 from './Lesson4';
+import Lesson4 from './Lesson4';
 
-
-const lessons = [
-    Lesson1, 
-    Lesson2, 
-    Lesson3,
-    Lesson4,
-];
+const lessons = [Lesson1, Lesson2, Lesson3, Lesson4, Lesson5, Lesson6];
 
 export default lessons;
